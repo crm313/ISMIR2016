bb8e48f6f3c30930ceadac20c00b1ea1f971e94f
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-03-24 01:52:58 +0100

    update invariance_ratio

--- invariance_ratio.m
+++ invariance_ratio.m
@@ -1,6 +1,13 @@
-function ratio = invariance_ratio(c)
+function ratio = invariance_ratio(c, tau)
+if nargin < 2
+    tau = 0;
+end
 c = cell2mat(c);
-dist_to2 = norm(c(:, 3) - c(:, 8));
-dist_to3 = norm(c(:, 3) - c(:, 13));
-ratio = dist_to3 ./ dist_to2; % bigger is better
+t1 = 2;
+t2 = 7;
+t3 = 12;
+dist_to2 = norm(c(:, t1 + tau) - c(:, t2 + tau));
+dist_to3 = norm(c(:, t1 + tau) - c(:, t3 + tau));
+ratio = dist_to3 ./ dist_to2 % bigger is better
+imagesc(c);
 end
